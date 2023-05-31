c33c4cd918f2a0c55ed8efec0906f5eaee8519cd
ppj-555 <Prasanna.Prakhar@shell.com>
2023-05-31 19:13:12 +0530

    smothing added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/The FE sdsdwd/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("renders learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/The FE/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("another text on app", () => {
+    render(<App />);
+    const text = screen.getByText(/Testing CI - push on main/i);
+    expect(text).toBeInTheDocument();
+  });
 });
